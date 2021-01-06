6e4a3055cc004fbe39737e2dda2d14aa220d6a36
Andres Fernando Narvaez Torres <plfxde02@coomeva.com.co>
2021-01-06 18:03:56 -0500

    primer commit

---
+++ src/app/pagesnopagefound/pagesnopagefound.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagesnopagefound',
+  templateUrl: './pagesnopagefound.component.html',
+  styleUrls: ['./pagesnopagefound.component.css']
+})
+export class PagesnopagefoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
